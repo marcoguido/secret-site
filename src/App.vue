40baa195bef519f703cb93b39a69cc799eae5708
Marco Guidolin <guidolin.marco94@gmail.com>
2026-09-10 15:35:42 +0200

    feat: time based variations (#14)

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,18 @@ import Timeline from '@/sections/Timeline.vue'
 import Carousel from '@/sections/Carousel.vue'
 import Gifts from '@/sections/Gifts.vue'
 import Rsvp from '@/sections/Rsvp.vue'
+import { ref } from 'vue'
+
+const now = ref(new Date())
+
+const targetDate = new Date('2026-11-14T11:30:00')
+const thankYouDate = new Date('2026-11-15T01:00:00')
+const rsvpDeadlineDate = new Date('2026-10-25T23:59:59')
 </script>
 
 <template>
   <div class="min-h-screen bg-cream-background text-powder-blue">
-    <hero />
+    <hero :target-date :thank-you-date />
 
     <logistics />
 
@@ -17,7 +24,7 @@ import Rsvp from '@/sections/Rsvp.vue'
 
     <gifts />
 
-    <rsvp />
+    <rsvp v-if="now.getTime() <= rsvpDeadlineDate.getTime()" :rsvp-deadline-date />
 
     <carousel />
   </div>
